5501d54f9f3f723bf7823d65d8309c1c6a9882c0
Engineer Torikul Islam <52188242+engineertorikulislam@users.noreply.github.com>
2022-01-03 13:04:37 +0600

    OS LAB FINAL PROJECT

---
+++ fifo.sh
@@ -0,0 +1,77 @@
+# Project Title : FIFO (First In First Out Page Replacement Algorithm By Bash Script)
+
+# Group No: 9
+
+# Md. Torikul Islam , ID: 192002131
+
+echo "Enter the number of pages : "             # Input  Total Number of Page 
+read page
+
+echo "Enter the Page Numbers : "
+    for (( i = 0; i < page; i++)); do                 
+        echo "page  $i "                                            # Show every index pases 
+        read value[$i]                                                #Store Pages in value array 
+    done
+
+echo "Enter the  number of frame : "            # Input the frame size
+read frame
+
+temp[$frame]=""                                              # Temp Innitialize(Rearrange Frame)
+ 
+    for ((i = 0; i < frame; i++));                          # Here First of All, We initialized frame index -1 
+    do
+        temp[$i]="-1"
+    done
+
+     echo "====== Page Frames ====== "
+
+for ((i = 0; i < page; i++)); 
+do
+    count=0                                                               # Counter Initialized
+    for (( j = 0; j < frame; j++)); 
+    do
+        if [ "${value[$i]}" == "${temp[$j]}" ]; then  # Push pages in the queue one at a time until the queue reaches its maximum capacity or all page requests are fulfilled.
+
+            let count++
+
+            let page_fault--                          # Decrease Page Miss (Page fault) 
+        fi
+    done
+
+    let page_fault++            # Increment the page faults
+
+    if [[ $page_fault -le $frame ]] && [[ $count -eq 0 ]]; then
+
+        let $((temp[i] = value[i]))                                # IPush pages in the queue one at a time until the queue reaches its maximum capacity or all page requests are fulfilled.
+
+    elif [[ $count -eq 0 ]]; then
+
+        let $((temp[(page_fault - 1) % frame] = value[i]))  # Replace the topmost page with the current page from the string
+    fi
+    echo " "
+    echo "========================"
+
+        for ((j = 0; j < frame; j++));
+         do
+            echo  "${temp[$j]}" 
+        done | column
+
+done
+
+echo " "
+echo "Total Page Fault :  $page_fault"                              # Page Fault Print 
+
+hit=`echo "$page-$page_fault" | bc`                                 # Page Hit Calculate
+
+echo "Total Page Hits : " $hit                                               # Page Hit Calculate Print 
+
+ hitrate=`echo "scale=2;  $hit/ $page*100 "  | bc`         # Calculate Hit Rate 
+
+echo "Hit Rate :" $hitrate                                                      # Print Hit Rape
+
+ faultrate=`echo "scale=2;  $page_fault/ $page *100 "  | bc` # Page Falut Rate calculate 
+
+ echo "Page Fault Rate : " $faultrate                                  # Page fault rate Print 
+
+
+
